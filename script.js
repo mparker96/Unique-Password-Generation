4728f95461c8995e99ce555473a6c672262767f4
mparker96 <93836748+mparker96@users.noreply.github.com>
2022-09-16 12:56:15 -0400

    added varibles for criteria information, added confirm prompts to select which criteria. future commits will see criteria selections be generated and prevent defaults to keep page from refreshing

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
+
+var specialCharacters = " !\"#$%&'()*+,-./:;`{|}~<=>?@[]^_";
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var optionsVariable = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -8,8 +15,23 @@ function writePassword() {
 
   passwordText.value = password;
 
+  event.preventDefault();
 }
-
+;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lowercaseLett = confirm(
+  "Do you want your password to contain lowercase letters?"
+);
+var uppercaseLett = confirm(
+  "Do you want your password to contain uppercase letters?"
+);
+var num = confirm("Do you want your password to contain numbers?");
+var specialChar = confirm(
+  "Do you want your password to contain special characters?"
+);
+var passwordLength = prompt(
+  "Choose a password length of at least 8 characters and no more than 128 characters."
+);
 
